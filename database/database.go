25e2328a003aa24fe6ef382a0036d0a708a4d25e
Agung <agungsusilo683@gmail.com>
2024-01-12 23:13:39 +0700

    files

---
+++ database/database.go
@@ -0,0 +1,18 @@
+package database
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// var DB *gorm.DB
+
+func ConnectDatabase() *gorm.DB {
+	database, err := gorm.Open(mysql.Open("root:agung@tcp(localhost:3306)/belajar_golang"))
+	if err != nil {
+		panic(err)
+	}
+
+	return database
+
+}
